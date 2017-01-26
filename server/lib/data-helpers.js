14f73f81cdc4fb190e7c96896e562995a4362e12
Kapish <kapish.k.1@gmail.com>
2017-01-26 16:04:01 -0500

    added mongo and tweets saving to db

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,26 +1,34 @@
 "use strict";
+const MongoClient = require("mongodb").MongoClient;
 
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
 
+
+// function makeDataHelpers(db)
+
+
 // Defines helper functions for saving and getting tweets, using the database `db`
-module.exports = function makeDataHelpers(db) {
+module.exports = (db) => {
+  const tweets = db.collection("tweets");
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        // db.tweets.push(newTweet);
+        tweets.insertOne(newTweet);
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      tweets.find({}).toArray(callback);
+      // simulateDelay(() => {
+      //   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      //   callback(null, db.tweets.sort(sortNewestFirst));
+      // });
     }
 
   };
